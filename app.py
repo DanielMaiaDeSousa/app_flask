09155a9b772aa42e1b42d318c4cccadde8825675
Daniel Maia <daniel.maia.sousa@gmail.com>
2025-11-04 20:12:51 -0300

    parametro_plask: Adiciona rota de saudação com parâmetro de rota

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 from dao.aluno_dao import AlunoDAO
 from dao.professor_dao import ProfessorDAO
 from dao.curso_dao import CursoDAO
@@ -10,11 +10,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/aluno')
+@app.route('/aluno/form')
 def listar_aluno():
     dao = AlunoDAO()
     lista = dao.listar()
     return render_template('aluno/lista.html', lista=lista)
+
+@app.route('/curso')
 
 @app.route('/professor')
 def listar_professor():
@@ -27,6 +29,24 @@ def listar_curso():
     dao = CursoDAO()
     lista = dao.listar()
     return render_template('curso/lista.html', lista=lista)
+
+@app.route('/saudacao/<nome>')
+def saudacao(nome):
+    return render_template('saudacao/saudacao.html', valor_recebido=nome)
+
+@app.route('/saudacao2/')
+def saudacao2():
+    nome = request.args.get('nome')
+    return render_template('saudacao/saudacao.html', valor_recebido=nome)
+
+@app.route('/login', methods=['POST'])
+def login():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    email = request.form['email']
+    # Aqui você pode adicionar a lógica de autenticação
+    dados = f"Usuário: {usuario}, Senha: {senha}, Email: {email}"
+    return render_template('saudacao/saudacao.html', valor_recebido=dados)
 
 if __name__ == '__main__':
     app.run(debug=True)
